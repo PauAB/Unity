f5b0314bcddc1bd1fa22bfc4508b467be324fe49
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2019-11-06 13:19:04 +0100

    Json
    Saving and loading objects into .json

---
+++ DireccionTecnologica/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    int maxPlayers = 5;
+
+    Player player;
+    Player player2;
+    Player player3;
+    Player[] players;
+
+    [SerializeField]
+    string jsonString;
+
+    void Start()
+    {
+        player = new Player();
+        player2 = new Player();
+        player3 = new Player();
+
+        players = new Player[maxPlayers];
+
+        player.SetPlayer("Player 1", 10f, 5f);
+        player2.SetPlayer("Player 2", 5f, 2.5f);
+        player3.SetPlayer("Player 3", 20f, 10f);
+
+        players[0] = player;
+        players[1] = player2;
+        players[2] = player3;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DataPersistenceManager.SaveJson(player, Application.dataPath + "/playerJson.json");
+            Debug.Log("Saved");
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            DataPersistenceManager.LoadJson<Player>(Application.dataPath + "/playerJson.json");
+            Debug.Log("Loaded");
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            DataPersistenceManager.SaveJsonArray(players, Application.dataPath + "/playersJson.json");
+            Debug.Log("Array Saved");
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            DataPersistenceManager.LoadJsonArray<Player>(Application.dataPath + "/playersJson.json");
+            Debug.Log("Array Loaded");
+        }
+    }
+}
---
+++ DireccionTecnologica/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player
+{
+    
+    public string playerName;    
+    public float playerPower;   
+    public float playerStrength;
+
+    public void SetPlayer(string name, float power, float strength)
+    {
+        playerName = name;
+        playerPower = power;
+        playerStrength = strength;
+    }
+}
