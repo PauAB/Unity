f5479977c3fbe18f069239c8ea5f8ca9c981119f
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2019-11-08 11:26:12 +0100

    Console

---
+++ DireccionTecnologica/Assets/Scripts/ConsoleManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class ConsoleManager : MonoBehaviour
+{
+    Dictionary<string, ConsoleFunction> consoleFunctions;
+
+    const char separator = ' ';    
+    string[] messageContainer;
+    string[] messageFuncName;
+    string[] messageParams;    
+    
+    void Start()
+    {
+        consoleFunctions = new Dictionary<string, ConsoleFunction>();
+        consoleFunctions.Add(nameof(SpawnOrc), new SpawnOrc());
+        DispatchMessage("/SpawnOrc 1 0 0");
+    }
+
+    void DispatchMessage(string mssg)
+    {
+        messageContainer = mssg.Split(separator);
+
+        if (messageContainer[0].StartsWith("/"))
+        {
+            messageFuncName = messageContainer[0].Split('/');            
+
+            if (messageFuncName[1] != "")
+            {
+                if (messageFuncName[1] == "help")
+                {
+                    foreach (ConsoleFunction cF in consoleFunctions.Values)
+                    {
+                        Debug.Log("Function " + cF.GetType().Name + " " + cF.FunctionArgumentsDefinition());
+                    }
+
+                    return;
+                }
+                else
+                {
+                    foreach (ConsoleFunction cF in consoleFunctions.Values)
+                    {
+                        if (messageFuncName[1] == cF.GetType().Name)
+                        {                            
+                            Debug.Log("With params: " + cF.Execute(messageContainer[1], messageContainer[2], messageContainer[3]));
+                            Debug.Log("With params: " + cF.Execute());
+                        }
+                    }
+                }
+            }
+            else Debug.Log("Invalid function");
+        }
+        else Debug.Log("Invalid function");
+    }
+
+}
---
+++ DireccionTecnologica/Assets/Scripts/SpawnOrc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOrc : ConsoleFunction
+{
+    string paramDefinition = "takes 3 parameters";
+    
+    public override bool Execute(params object[] parameters)
+    {        
+        if (parameters.GetLength(0) <= 0) return false;
+        else if (parameters.GetLength(0) > 0) return true;
+
+        return false;
+    }
+
+    public override string FunctionArgumentsDefinition()
+    {
+        return paramDefinition;
+    }
+
+   
+}
