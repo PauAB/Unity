cffc0ca515aa9296b04d06789ebe211d00bfb22b
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2020-01-29 08:45:29 +0100

    Automata Project

---
+++ Automata/Automata/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAgent : MonoBehaviour
+{
+    public delegate void stateFunction();
+    public Dictionary<string, stateFunction> states;
+
+    protected stateFunction currentState;
+
+    private void Awake()
+    {
+        states = new Dictionary<string, stateFunction>();
+    }
+
+    public virtual void UpdateAgent()
+    {
+
+    }
+}
---
+++ Automata/Automata/Assets/Scripts/AIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    
+    public AIAgent agent;    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // assert
+        agent.UpdateAgent();
+    }
+}
